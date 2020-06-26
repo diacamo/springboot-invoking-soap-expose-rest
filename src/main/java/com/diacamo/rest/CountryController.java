e0f9b6ec134f85fc65bbd3c17be183fa60b716db
Diacamo Mankungi <diacamosky@gmail.com>
2020-06-26 01:18:57 +0100

    fix swagger issue

--- src/main/java/com/diacamo/rest/CountryController.java
+++ src/main/java/com/diacamo/rest/CountryController.java
@@ -2,66 +2,65 @@ package com.diacamo.rest;
 
 
 import com.diacamo.soap.ws.client.CountryClient;
-import com.diacamo.rest.Country;
+//import com.diacamo.rest.Country;
+import com.diacamo.soap.ws.client.gen.Currency;
 import com.diacamo.soap.ws.client.gen.GetCountryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jnlp.ClipboardService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-//@RestController
-@Controller
-@RequestMapping(value = "/country")
+@RestController
+//@Controller
+//@RequestMapping(value = "/country")
 public class CountryController {
 
 //
 //    private static final String template = "Hello, %s!";
 //    //    private final AtomicLong counter = new AtomicLong();
-//    @Autowired
-//    CountryClient client;
+    @Autowired
+    CountryClient client;
 //
-//    private Country country;
+    private com.diacamo.soap.ws.client.gen.Country country;
 
-//    @GetMapping("/country/{name}")
+    @GetMapping("/country/{name}")
 //    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
 //    @ResponseBody
-//    public Country getCountry(@PathVariable(value = "name") String name) {
-//
-//        //return new Country(name);
-////        GetCountryResponse response = client.getCountry(name);
-////        country = new Country();
-////        country.setName(response.getCountry().getName());
-////        country.setCapital(response.getCountry().getCapital());
-////        return country;
-//
-//        country = client.getCountry(name).getCountry();
+    public com.diacamo.soap.ws.client.gen.Country getCountry(@PathVariable(value = "name") String name) {
+
+//        GetCountryResponse response = client.getCountry(name);
+//        country = new Country();
+//        country.setName(response.getCountry().getName());
+//        country.setCapital(response.getCountry().getCapital());
 //        return country;
-//
-//
-//    }
 
-    ConcurrentMap<String, Country> countries = new ConcurrentHashMap<>();
-
-    @GetMapping("/{name}")
-    public Country getCountry(@PathVariable String name) {
-        return countries.get(name);
-    }
-
-    @GetMapping("/")
-    public List<Country> getAllCountries () {
-        return new ArrayList<Country>(countries.values());
-    }
-
-    @GetMapping("/")
-    public Country addCountry(@RequestBody Country country) {
-        countries.put(country.getName(), country);
+        country = client.getCountry(name).getCountry();
         return country;
+
+
     }
+
+//    ConcurrentMap<String, Country> countries = new ConcurrentHashMap<>();
+//
+//    @GetMapping("/{name}")
+//    public Country getCountry(@PathVariable String name) {
+//        return countries.get(name);
+//    }
+//
+//    @GetMapping("/")
+//    public List<Country> getAllCountries () {
+//        return new ArrayList<Country>(countries.values());
+//    }
+//
+//    @GetMapping("/")
+//    public Country addCountry(@RequestBody Country country) {
+//        countries.put(country.getName(), country);
+//        return country;
+//    }
 
 }
 
